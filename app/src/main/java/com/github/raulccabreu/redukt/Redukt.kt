9d926a315e0eeaa24a0ab25ee7df538eda4ac1eb
niltonvasques <nilton.vasques@gmail.com>
2017-12-11 17:32:46 -0300

    - [x] Create a first naive dispatch inside Redukt

--- app/src/main/java/com/github/raulccabreu/redukt/Redukt.kt
+++ app/src/main/java/com/github/raulccabreu/redukt/Redukt.kt
@@ -1,9 +1,17 @@
 package com.github.raulccabreu.redukt
 
+import com.github.raulccabreu.redukt.actions.Action
 import com.github.raulccabreu.redukt.reducers.Reducer
 
 class Redukt<T: Any>(state: T) {
     var state = state
         private set
     val reducers = mutableSetOf<Reducer<T>>()
+
+    fun dispatch(action: Action<*>) {
+        // Naive non async approach to reduce the state
+        var tempState = state
+        reducers.forEach { tempState = it.reduce(tempState, action) }
+        state = tempState
+    }
 }
--- app/src/test/java/com/github/raulccabreu/redukt/ReduktTest.kt
+++ app/src/test/java/com/github/raulccabreu/redukt/ReduktTest.kt
@@ -5,9 +5,6 @@ import com.github.raulccabreu.redukt.reducers.Reducer
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
-/**
- * Created by niltonvasques on 12/11/17.
- */
 class ReduktTest {
     @Test
     fun initialState() {
@@ -25,5 +22,37 @@ class ReduktTest {
         redukt.reducers.add(reducer)
         assertEquals(1, redukt.reducers.size)
         assertEquals(reducer, redukt.reducers.first())
+    }
+
+    @Test
+    fun afterDispatch() {
+        val redukt = Redukt<String>("initial")
+        val reducer = object: Reducer<String> {
+            override fun reduce(state: String, action: Action<*>) = action.payload.toString()
+        }
+        redukt.reducers.add(reducer)
+        assertEquals("initial", redukt.state)
+        redukt.dispatch(Action("action", "new state"))
+        assertEquals("new state", redukt.state)
+        redukt.dispatch(Action("action", "another state"))
+        assertEquals("another state", redukt.state)
+    }
+
+    @Test
+    fun afterDispatchWithTwoReducers() {
+        val redukt = Redukt<String>("initial")
+        val changerReducer = object: Reducer<String> {
+            override fun reduce(state: String, action: Action<*>) = action.payload.toString()
+        }
+        val upcaseReducer = object: Reducer<String> {
+            override fun reduce(state: String, action: Action<*>) = state.toUpperCase()
+        }
+        redukt.reducers.add(changerReducer)
+        redukt.reducers.add(upcaseReducer)
+        assertEquals("initial", redukt.state)
+        redukt.dispatch(Action("action", "new state"))
+        assertEquals("NEW STATE", redukt.state)
+        redukt.dispatch(Action("action", "another state"))
+        assertEquals("ANOTHER STATE", redukt.state)
     }
 }
