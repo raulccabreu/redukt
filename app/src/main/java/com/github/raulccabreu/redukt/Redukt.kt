6a981fa75ec8f13de00b48c9df9085f75d9051d3
niltonvasques <nilton.vasques@gmail.com>
2017-12-13 14:38:20 -0300

    Fix concurrent modification exceptions

--- app/src/main/java/com/github/raulccabreu/redukt/Redukt.kt
+++ app/src/main/java/com/github/raulccabreu/redukt/Redukt.kt
@@ -32,6 +32,8 @@ class Redukt<T>(state: T) {
 
     private fun reduce(action: Action<*>) {
         val elapsed = measureTimeMillis {
+            val listeners = listeners.toSet() //to avoid concurrent modification exception
+            val middlewares = middlewares.toSet()
             val oldState = state
             var tempState = state
             middlewares.parallelFor { it.before(tempState, action) }
